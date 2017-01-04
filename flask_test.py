78f127b3986b50b1d7a571567fd45298849b392f
hujun1413 <359328242@qq.com>
2017-01-04 16:04:50 +0800

    add one page

--- flask_test.py
+++ flask_test.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request,send_from_directory,abort,jsonify
+from werkzeug import secure_filename
+import os
 
 app = Flask(__name__)
-
+UPLOAD_FOLDER = 'static/upload'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    return render_template('index.html')
 
+@app.route('/test1/')
+def test1():
+    return render_template('1.html')
+
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        fname = secure_filename(f.filename)
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        file_dir=os.path.join(basedir, UPLOAD_FOLDER)
+        f.save(os.path.join(file_dir, fname))
+        return 'success!'
+    return
 
 if __name__ == '__main__':
     #app.run(host='127.0.0.1')
